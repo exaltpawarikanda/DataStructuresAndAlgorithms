13ad971b4fabab386f00acb921ec7499e9289ebe
exalt <exaltpawa@gmail.com>
2022-06-02 22:55:45 +0200

    String reverse using arrays

---
+++ src/com/exaltpawarikanda/solutions/datastructures/array/StringReverse.java
@@ -0,0 +1,28 @@
+package com.exaltpawarikanda.solutions.datastructures.array;
+
+public class StringReverse {
+
+    public static void main(String[] args){
+
+        reverse("How are you");
+
+    }
+
+
+    public static void reverse(String str){
+
+        if(str.length() < 2){
+            System.out.println("Please provide a string with length greater than 2 !!!");
+        }else{
+            char[] strCharacters = new char[str.length()];
+
+            for(int i = 0; i < str.length(); i++){
+                strCharacters[i] = str.charAt((str.length()-1) - i);
+            }
+
+            System.out.println(String.valueOf(strCharacters));
+        }
+
+
+    }
+}
